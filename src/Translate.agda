llvm-agda/Verilette

commit 7fcc3a2a20018006a04c9ee090975f8b4316cca0

Changed index of Stm to Ctx to Ctx (instead of to Block) for nicer composition

src/Translate.agda
@@ -17,7 +17,7 @@ open import Function using (_∘_; const)
 
 open import WellTyped
 open import Javalette.AST using (Type; Ident; Item; plus; minus); open Type; open Item
-open import TypedSyntax hiding (Γ; Δ; Δ') renaming (Block to newBlock; Ctx to newCtx)
+open import TypedSyntax hiding (Γ; Γ'; Δ; Δ') renaming (Block to newBlock; Ctx to newCtx)
 
 
 -- Translating from WellTyped to TypedSyntax
@@ -37,13 +37,19 @@ dropAllId' = map proj₂
 dropAllId : Ctx → newCtx
 dropAllId = map dropAllId'
 
-simplifyLookup' : (id , t) ∈ Δ → t ∈ (dropAllId' Δ)
-simplifyLookup' (here refl) = here refl
-simplifyLookup' (there x) = there (simplifyLookup' x)
+module _ where
+  private
+    variable
+      P : A → Set
+      Q : B → Set
+      f : A → B
+
+  anyMap : (∀ {x} → P x → Q (f x)) → Any P xs → Any Q (map f xs)
+  anyMap F (here x)   = here (F x)
+  anyMap F (there xs) = there (anyMap F xs)
 
 simplifyLookup : (id , t) ∈' Γ → t ∈' (dropAllId Γ)
-simplifyLookup (here px) = here (simplifyLookup' px)
-simplifyLookup (there x) = there (simplifyLookup x)
+simplifyLookup = anyMap (anyMap λ {refl → refl})
 
 zero : Num T → toSet T
 zero NumInt    = Int.0ℤ
@@ -97,19 +103,19 @@ defInit NonVoidBool = EValue Bool.false
 defInit (NonVoidArray  _) = EArray (nType (EValue Int.0ℤ))
 defInit (NonVoidStruct _) = EValue 0
 
-toDecls : ∀ {is t} → NonVoid χ t → DeclP t (Δ ∷ Γ) is Δ' → Stms T (dropAllId ((Δ' ++r Δ) ∷ Γ)) → Stms T (dropAllId (Δ ∷ Γ))
-toDecls n  noDecl          ss = ss
-toDecls n (noInit id   ds) ss = SDecl _ (defInit n) ∷ toDecls n ds ss
-toDecls n (init   id e ds) ss = SDecl _ (toExp e)   ∷ toDecls n ds ss
+toDecls : ∀ {is t} → NonVoid χ t → Star' (DeclP t) Γ is Γ' → Stms T (dropAllId Γ') → Stms T (dropAllId Γ)
+toDecls n  []                ss = ss
+toDecls n (noInit id   ∷ ds) ss = SDecl _ (defInit n) ∷ toDecls n ds ss
+toDecls n (init   id e ∷ ds) ss = SDecl _ (toExp e)   ∷ toDecls n ds ss
 
 
-toStms   : ∀ {ss} → _⊢_⇒⇒_ T (Δ ∷ Γ) ss Δ'                                      → Stms T (dropAllId (Δ ∷ Γ))
-_SCons'_ : ∀ {s}  → _⊢_⇒_  T (Δ ∷ Γ) s  Δ' → Stms T (dropAllId ((Δ' ++ Δ) ∷ Γ)) → Stms T (dropAllId (Δ ∷ Γ))
-toStms (x ∷ ss) = x SCons' (toStms ss)
-toStms {T = void} {Γ = []} [] = SReturn vRet ∷ []
-toStms {_}        {_}      [] = []
+toStms   : ∀ {ss} → _⊢_⇒⇒_ T Γ ss Γ'                         → Stms T (dropAllId Γ)
+_SCons'_ : ∀ {s}  → _⊢_⇒_  T Γ s  Γ' → Stms T (dropAllId Γ') → Stms T (dropAllId Γ)
+toStms (x ∷ ss)                  = x SCons' (toStms ss)
+toStms {T = void} {Γ = _ ∷ _} [] = SReturn vRet ∷ []
+toStms {_}        {_}         [] = []
 
-_SCons'_ {Δ = Δ} (decl {Δ' = Δ'} n is) ss rewrite sym (ʳ++-defn Δ' {Δ}) = toDecls n is ss
+decl n is      SCons' ss = toDecls n is ss
 empty          SCons' ss = ss
 bStmt x        SCons' ss = SBlock (toStms x) ∷ ss
 ass id x e     SCons' ss = SAss (simplifyLookup x) (toExp e) ∷ ss

src/TypeCheck/CheckExp.agda
@@ -80,7 +80,7 @@ module CheckExp (Γ : Ctx) where
   infer (eAttrib e (ident x₁)) = error $ "Found non-legal attribute: " ++s x₁
   infer (eDeRef e x) = do e' ::: structT n ← infer e
                              where e' ::: _ → error "Tried to deref non-struct"
-                          ( c , fs ) , p ← lookupTCM n χ
+                          (c , fs) , p ← lookupTCM n χ
                           t , p' ← lookupTCM x fs
                           pure (eDeRef e' p p' ::: t)
   infer (neg e) = do e' ::: t ← infer e
@@ -133,67 +133,67 @@ module CheckStatements (T : Type) where
   check     : (Γ : Ctx) → (s  :      Stmt) → TCM (∃ (Γ ⊢ s ⇒_))
 
   checkStms : (Γ : Ctx) → (ss : List Stmt) → TCM (∃ (Γ ⊢ ss ⇒⇒_))
-  checkStms Γ [] = pure ([] , [])
-  checkStms Γ (s ∷ ss) = do Δ  , s'  ← check Γ s
-                            Δ' , ss' ← checkStms (Δ ,, Γ) ss
-                            pure (Δ' ++ Δ , s' ∷ ss')
+  checkStms Γ [] = pure (_ , [])
+  checkStms Γ (s ∷ ss) = do _ , s'  ← check _ s
+                            _ , ss' ← checkStms _ ss
+                            pure (_ , s' ∷ ss')
 
-  check Γ empty = pure ([] , empty)
+  check Γ empty = pure (_ , empty)
   check Γ (bStmt (block ss)) = do _ , ss' ← checkStms ([] ∷ Γ) ss
-                                  pure ([] , bStmt ss')
+                                  pure (_ , bStmt ss')
 
   check Γ (ass (eVar x) e) = do t , p ← lookupCtx x Γ
                                 e' ← checkExp Γ t e
-                                pure ([] , ass x p e')
+                                pure (_ , ass x p e')
   check Γ (ass (eIndex arr i) x) = do i'     ← checkExp Γ int i
                                       t , x' ← infer Γ x
                                       arr'   ← checkExp Γ (array t) arr
-                                      pure ([] , assIdx arr' i' x')
+                                      pure (_ , assIdx arr' i' x')
   check Γ (ass (eDeRef e f) x) = do e' ::: structT t ← infer Γ e
                                        where e' ::: _ → error "Can not defer field from non-struct type"
                                     (c , fs) , p ← lookupTCM t χ
                                     t' , p' ← lookupTCM f fs
                                     x' ← checkExp Γ t' x
-                                    pure ([] , assPtr e' p p' x')
+                                    pure (_ , assPtr e' p p' x')
   check Γ (ass _ e) = error "Could not assign to expression"
   check Γ (incr x) = do int , p ← lookupCtx x Γ
                           where _ → error "Can not increment non-int type"
-                        pure ([] , incr x p)
+                        pure (_ , incr x p)
   check Γ (decr x) = do int , p ← lookupCtx x Γ
                           where _ → error "Can not decrement non-int type"
-                        pure ([] , decr x p)
+                        pure (_ , decr x p)
   check Γ (ret e) = do e' ← checkExp Γ T e
-                       pure ([] , ret e')
+                       pure (_ , ret e')
   check Γ vRet = do refl ← T =?= void
-                    pure ([] , vRet refl)
+                    pure (_ , vRet refl)
   check Γ (cond e t) = do e' ← checkExp Γ bool e
                           _ , t' ← check ([] ∷ Γ) t
-                          pure ([] , cond e' t')
+                          pure (_ , cond e' t')
   check Γ (condElse e t f) = do e' ← checkExp Γ bool e
                                 _ , t' ← check ([] ∷ Γ) t
                                 _ , f' ← check ([] ∷ Γ) f
-                                pure ([] , condElse e' t' f')
+                                pure (_ , condElse e' t' f')
   check Γ (while e s) = do e' ← checkExp Γ bool e
                            _ , s' ← check ([] ∷ Γ) s
-                           pure ([] , while e' s')
+                           pure (_ , while e' s')
   check Γ (for t id e s) = do e' ← checkExp Γ (array t) e
                               _ , s' ← check ([ id , t ] ∷ Γ) s
-                              pure ([] , (for id e' s'))
+                              pure (_ , (for id e' s'))
   check Γ (sExp e) = do e' ← checkExp Γ void e
-                        pure ([] , sExp e')
+                        pure (_ , sExp e')
   check Γ (decl t is) with Γ
   ... | []    = error "Can not declare variable in empty Ctx"
   ... | Δ ∷ Γ = do p ← ifNonVoid χ t
                    Δ' , is' ← checkIs Δ is
-                   pure (reverse Δ' , decl p is')
-    where checkIs : ∀ Δ → (is : List Item) → TCM (∃ (DeclP t (Δ ∷ Γ) is))
-          checkIs Δ [] = pure ([] , noDecl)
+                   pure (_ , decl p is')
+    where checkIs : ∀ Δ → (is : List Item) → TCM (∃ (Star' (DeclP t) (Δ ∷ Γ) is))
+          checkIs Δ [] = pure (_ , [])
           checkIs Δ (noInit id ∷ is) = do p  ← id notIn Δ
                                           Δ' , ps ← checkIs ((id , t) ∷ Δ) is
-                                          pure ((id , t) ∷ Δ' , noInit p ps)
+                                          pure (_ , noInit p ∷ ps)
           checkIs Δ (init id e ∷ is) = do p , e'  ← zipM (id notIn Δ) (checkExp (Δ ∷ Γ) t e)
                                           Δ' , ps ← checkIs ((id , t) ∷ Δ) is
-                                          pure ((id , t) ∷ Δ' , init p e' ps)
+                                          pure (_ , init p e' ∷ ps)
 
 
 module _ where
@@ -201,8 +201,8 @@ module _ where
   open Statements Σ χ
   open WellTyped.Return
 
-  checkReturn  : ∀ {ss t} → (ss' : _⊢_⇒⇒_ t Γ ss Δ) → TCM (Returns  ss')
-  checkReturn' : ∀ {s  t} → (s'  : _⊢_⇒_  t Γ s  Δ) → TCM (Returns' s')
+  checkReturn  : ∀ {ss t} → (ss' : _⊢_⇒⇒_ t Γ ss Γ') → TCM (Returns  ss')
+  checkReturn' : ∀ {s  t} → (s'  : _⊢_⇒_  t Γ s  Γ') → TCM (Returns' s')
   checkReturn (s ∷ ss) = here <$> checkReturn' s <|> there <$> checkReturn ss
   checkReturn {Γ = Δ ∷ []} {t = void} [] = pure vEnd
   checkReturn                         [] = error "CheckReturn failed; found no return"

src/TypeCheck/Proofs.agda
@@ -106,15 +106,15 @@ module ReturnsProof (Σ : SymbolTab) (χ : TypeTab) where
   open import Translate Σ χ using (dropAllId; toExp; toStms; _SCons'_; toDecls)
 
 
-  returnDecl : ∀ {T Γ Δ Δ' t is} (n : TS.NonVoid χ t)
-               {ss : Stms T (dropAllId ((Δ' ++r Δ) ∷ Γ))}
-               (is' : DeclP t (Δ ∷ Γ) is Δ')
+  returnDecl : ∀ {T Γ t is} (n : TS.NonVoid χ t)
+               {ss : Stms T (dropAllId Γ')}
+               (is' : Star' (DeclP t) Γ is Γ')
                     → TS.returnStms ss → TS.returnStms (toDecls n is' ss)
-  returnDecl n noDecl p = p
-  returnDecl n (noInit px   is) p = SCon (returnDecl n is p)
-  returnDecl n (init   px e is) p = SCon (returnDecl n is p)
+  returnDecl n [] p = p
+  returnDecl n (noInit px   ∷ is) p = SCon (returnDecl n is p)
+  returnDecl n (init   px e ∷ is) p = SCon (returnDecl n is p)
 
-  returnProofThere : ∀ {T s ss Δ Δ' Δ''} {sT : _⊢_⇒_ T (Δ ∷ Γ) s Δ'} {ssT : _⊢_⇒⇒_ T _ ss Δ''}
+  returnProofThere : ∀ {T s ss} {sT : _⊢_⇒_ T Γ s Γ'} {ssT : _⊢_⇒⇒_ T Γ' ss Γ''}
                             → TS.returnStms (toStms ssT) → TS.returnStms (toStms (sT ∷ ssT))
   returnProofThere {sT = empty} x = x
   returnProofThere {sT = ret x₁} x       = SHead SReturn
@@ -130,12 +130,11 @@ module ReturnsProof (Σ : SymbolTab) (χ : TypeTab) where
   returnProofThere {sT = for id x₁ sT} x = SCon x
   returnProofThere {sT = sExp x₁} x      = SCon x
   returnProofThere {sT = assPtr x₁ x₂ x₃ x₄} x = SCon x
-  returnProofThere {Δ = Δ} {sT = decl {Δ' = Δ'} n is} x
-                       rewrite sym (ʳ++-defn Δ' {Δ}) = returnDecl n is x -- Why is this rewrite necessary?
+  returnProofThere {sT = decl n is} x = returnDecl n is x
 
 
-  returnProof     : ∀ {T ss}    {ssT : _⊢_⇒⇒_ T (Δ ∷ Γ) ss Δ'} → Returns ssT → TS.returnStms (toStms ssT)
-  returnProofHere : ∀ {T s ssT} {sT  : _⊢_⇒_  T (Δ ∷ Γ) s  Δ'} → Returns' sT → TS.returnStms (sT SCons' ssT)
+  returnProof     : ∀ {T ss}    {ssT : _⊢_⇒⇒_ T Γ ss Γ'} → Returns ssT → TS.returnStms (toStms ssT)
+  returnProofHere : ∀ {T s ssT} {sT  : _⊢_⇒_  T Γ s  Γ'} → Returns' sT → TS.returnStms (sT SCons' ssT)
   returnProofHere ret       = SHead SReturn
   returnProofHere vRet      = SHead SReturn
   returnProofHere (bStmt x) = SHead (SBlock (returnProof x))

src/WellTyped.agda
@@ -39,7 +39,7 @@ variable
   e x y : Expr
   es : List Expr
   Δ Δ' : Block
-  Γ : Ctx
+  Γ Γ' Γ'' : Ctx
 
 
 data OrdOp : RelOp → Set where
@@ -97,18 +97,20 @@ module Expression (Σ : SymbolTab) (χ : TypeTab) where
     ePrintString : ∀ s → Γ ⊢ eApp (ident "printString") (eString s ∷ []) ∶ void
 
 
-_,,_ : Block → Ctx → Ctx
-Δ ,, [] = Δ ∷ []
-Δ ,, (Δ' ∷ Γ) = (Δ ++ Δ') ∷ Γ
 
+-- Reflexiv Transitive closure over list, i.e. a chain
+module _ {A O : Set} (P : A → O → A → Set) where
+
+  data Star' (x : A) : List O → A → Set where
+    []  : Star' x [] x
+    _∷_ : ∀ {o os y z} → P x o y → Star' y os z → Star' x (o ∷ os) z
 
 module Declarations (Σ : SymbolTab) (χ : TypeTab) (t : Type) where
   open Expression Σ χ
 
-  data DeclP : (Γ : Ctx) → List Item → Block → Set where
-    noDecl : DeclP Γ [] []
-    noInit : ∀ {id   is} → id ∉ Δ                   → DeclP (((id , t) ∷ Δ) ∷ Γ) is Δ' → DeclP (Δ ∷ Γ) (noInit id ∷ is) ((id , t) ∷ Δ')
-    init   : ∀ {id e is} → id ∉ Δ → (Δ ∷ Γ) ⊢ e ∶ t → DeclP (((id , t) ∷ Δ) ∷ Γ) is Δ' → DeclP (Δ ∷ Γ) (init id e ∷ is) ((id , t) ∷ Δ')
+  data DeclP : Ctx → Item → Ctx → Set where
+    noInit : ∀ {id  } → id ∉ Δ                   → DeclP (Δ ∷ Γ) (noInit id) (((id , t) ∷ Δ) ∷ Γ)
+    init   : ∀ {id e} → id ∉ Δ → (Δ ∷ Γ) ⊢ e ∶ t → DeclP (Δ ∷ Γ) (init id e) (((id , t) ∷ Δ) ∷ Γ)
 
 
 module Statements (Σ : SymbolTab) (χ : TypeTab) (T : Type) where
@@ -116,27 +118,28 @@ module Statements (Σ : SymbolTab) (χ : TypeTab) (T : Type) where
   open Declarations Σ χ
 
 
-  data _⊢_⇒⇒_ (Γ : Ctx) : List Stmt → Block → Set
-  data _⊢_⇒_  (Γ : Ctx) :      Stmt → Block → Set where
-    empty  : Γ ⊢ empty ⇒ []
-    sExp   : Γ ⊢ e ∶ void  →  Γ ⊢ sExp e ⇒ []
-    bStmt  : ∀ {ss} → ([] ∷ Γ) ⊢ ss ⇒⇒ Δ → Γ ⊢ bStmt (block ss) ⇒ []
-    decl   : ∀ {t is} → NonVoid χ t → DeclP t Γ is Δ' → Γ ⊢ decl t is ⇒ reverse Δ'
-    ass    : ∀ {t} id → (id , t) ∈' Γ  →  Γ ⊢ e ∶ t    →  Γ ⊢ ass (eVar id) e ⇒ []
-    assIdx : ∀ {t arr i x}  → Γ ⊢ arr ∶ array t →  Γ ⊢ i ∶ int →  Γ ⊢ x ∶ t  →  Γ ⊢ ass (eIndex arr i) x ⇒ []
-    assPtr : ∀ {t s e fs f n c} → Γ ⊢ s ∶ structT n → (n , c , fs) ∈ χ → (f , t) ∈ fs → Γ ⊢ e ∶ t → Γ ⊢ ass (eDeRef s f) e ⇒ []
-    incr   : ∀ id → (id , int) ∈' Γ  →  Γ ⊢ incr id ⇒ []
-    decr   : ∀ id → (id , int) ∈' Γ  →  Γ ⊢ decr id ⇒ []
-    ret    : Γ ⊢ e ∶ T  →  Γ ⊢ ret e ⇒ []
-    vRet   : T ≡ void    →  Γ ⊢ vRet  ⇒ []
-    cond     : ∀ {s}   → Γ ⊢ e ∶ bool →  ([] ∷ Γ) ⊢ s ⇒ Δ  →                       Γ ⊢ cond     e s   ⇒ []
-    condElse : ∀ {t f} → Γ ⊢ e ∶ bool →  ([] ∷ Γ) ⊢ t ⇒ Δ  →  ([] ∷ Γ) ⊢ f ⇒ Δ' →  Γ ⊢ condElse e t f ⇒ []
-    while    : ∀ {s}   → Γ ⊢ e ∶ bool →  ([] ∷ Γ) ⊢ s ⇒ Δ →                        Γ ⊢ while e s ⇒ []
-    for      : ∀ {t e s} id →  Γ ⊢ e ∶ array t  →  ([ id , t ] ∷ Γ) ⊢ s ⇒ Δ'  →  Γ ⊢ for t id e s ⇒ []
+  data _⊢_⇒_  (Γ : Ctx) :    Stmt → Ctx → Set
 
-  data _⊢_⇒⇒_ Γ where
-    []  : Γ ⊢ [] ⇒⇒ []
-    _∷_ : ∀ {s ss} → Γ ⊢ s ⇒ Δ  →  (Δ ,, Γ) ⊢ ss ⇒⇒ Δ'  →  Γ ⊢ s ∷ ss ⇒⇒ (Δ' ++ Δ)
+  -- Reflexive transitive closure of _⊢_⇒_, i.e. a link of statments
+  _⊢_⇒⇒_ : (Γ : Ctx) → List Stmt → Ctx → Set
+  _⊢_⇒⇒_ = Star' _⊢_⇒_
+
+  data _⊢_⇒_ Γ where
+    empty  : Γ ⊢ empty ⇒ Γ
+    sExp   : Γ ⊢ e ∶ void  →  Γ ⊢ sExp e ⇒ Γ
+    bStmt  : ∀ {ss} → ([] ∷ Γ) ⊢ ss ⇒⇒ Γ' → Γ ⊢ bStmt (block ss) ⇒ Γ
+    decl   : ∀ {t is} → NonVoid χ t → Star' (DeclP t) Γ is Γ' → Γ ⊢ decl t is ⇒ Γ'
+    ass    : ∀ {t} id → (id , t) ∈' Γ  →  Γ ⊢ e ∶ t    →  Γ ⊢ ass (eVar id) e ⇒ Γ
+    assIdx : ∀ {t arr i x}  → Γ ⊢ arr ∶ array t →  Γ ⊢ i ∶ int →  Γ ⊢ x ∶ t  →  Γ ⊢ ass (eIndex arr i) x ⇒ Γ
+    assPtr : ∀ {t s e fs f n c} → Γ ⊢ s ∶ structT n → (n , c , fs) ∈ χ → (f , t) ∈ fs → Γ ⊢ e ∶ t → Γ ⊢ ass (eDeRef s f) e ⇒ Γ
+    incr   : ∀ id → (id , int) ∈' Γ  →  Γ ⊢ incr id ⇒ Γ
+    decr   : ∀ id → (id , int) ∈' Γ  →  Γ ⊢ decr id ⇒ Γ
+    ret    : Γ ⊢ e ∶ T  →  Γ ⊢ ret e ⇒ Γ
+    vRet   : T ≡ void    →  Γ ⊢ vRet  ⇒ Γ
+    cond     : ∀ {s}   → Γ ⊢ e ∶ bool →  ([] ∷ Γ) ⊢ s ⇒ Γ' →                        Γ ⊢ cond     e s   ⇒ Γ
+    condElse : ∀ {t f} → Γ ⊢ e ∶ bool →  ([] ∷ Γ) ⊢ t ⇒ Γ' →  ([] ∷ Γ) ⊢ f ⇒ Γ'' →  Γ ⊢ condElse e t f ⇒ Γ
+    while    : ∀ {s}   → Γ ⊢ e ∶ bool →  ([] ∷ Γ) ⊢ s ⇒ Γ' →                        Γ ⊢ while e s ⇒ Γ
+    for      : ∀ {t e s} id →  Γ ⊢ e ∶ array t  →  ([ id , t ] ∷ Γ) ⊢ s ⇒ Γ'  →  Γ ⊢ for t id e s ⇒ Γ
 
 
 -- Witness of return statments
@@ -150,13 +153,13 @@ module Return {Σ : SymbolTab} {χ : TypeTab} where
       s s1 s2 : Stmt
       ss      : List Stmt
 
-      s' s1' s2' : _⊢_⇒_  T Γ s Δ
-      ss' : _⊢_⇒⇒_ T Γ ss Δ
+      s' s1' s2' : _⊢_⇒_  T Γ s Γ'
+      ss' : _⊢_⇒⇒_ T Γ ss Γ'
 
       e' : Γ ⊢ e ∶ T
 
-  data Returns' {Γ : Ctx} : ∀ {  T} → (_⊢_⇒_  T Γ s   Δ) → Set
-  data Returns            : ∀ {Γ T} → (_⊢_⇒⇒_ T Γ ss  Δ) → Set where
+  data Returns' {Γ : Ctx} : ∀ {  T} → (_⊢_⇒_  T Γ s  Γ') → Set
+  data Returns            : ∀ {Γ T} → (_⊢_⇒⇒_ T Γ ss Γ') → Set where
     here  : Returns' s' → Returns (s' ∷ ss')
     there : Returns ss' → Returns (s' ∷ ss')
     vEnd  : Returns {Γ = Δ ∷ []} {T = void} []

commit 81f5c56612a30f01e9efe3d2cb026be03605de63

Fix bug with void return
Bug introduced in 7fcc3a2

src/Translate.agda
@@ -96,7 +96,7 @@ toDecls n (init   id e ∷ ds) ss = SDecl _ (toExp e)   ∷ toDecls n ds ss
 toStms   : ∀ {ss} → _⊢_⇒⇒_ T Γ ss Γ'                         → Stms T (dropAllId Γ)
 _SCons'_ : ∀ {s}  → _⊢_⇒_  T Γ s  Γ' → Stms T (dropAllId Γ') → Stms T (dropAllId Γ)
 toStms (x ∷ ss)                  = x SCons' (toStms ss)
-toStms {T = void} {Γ = _ ∷ _} [] = SReturn vRet ∷ []
+toStms {T = void} {Γ = _ ∷ []} [] = SReturn vRet ∷ []
 toStms {_}        {_}         [] = []
 
 decl n is      SCons' ss = toDecls n is ss